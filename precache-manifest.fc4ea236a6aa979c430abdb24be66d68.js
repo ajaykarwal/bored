64bbfdbe4481c242dc0f5b49602fe93ffe93a2e9
Ajay Karwal <ajay.karwal@ecx.io>
2019-02-07 15:28:53 +0000

    Updates

---
+++ precache-manifest.fc4ea236a6aa979c430abdb24be66d68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ebfdeaa1733c69e5819",
+    "url": "/bored/static/js/runtime~main.5ebfdeaa.js"
+  },
+  {
+    "revision": "eefa39616f6cc4ee735d",
+    "url": "/bored/static/js/main.eefa3961.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/bored/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "eefa39616f6cc4ee735d",
+    "url": "/bored/static/css/main.97d5fa9c.chunk.css"
+  },
+  {
+    "revision": "ba984e658bbd877935be48a44e9b01cd",
+    "url": "/bored/index.html"
+  }
+];
